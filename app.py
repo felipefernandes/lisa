1d5514a69388f151fc0fd0db2f9fce641700531d
Felipe Fernandes <felipefernandesweb@gmail.com>
2024-05-12 15:30:48 -0300

    organizacao de arquivos e pastas

--- app.py
+++ app.py
@@ -1,50 +1,55 @@
-!pip install -q -U google-generativeai
+# !pip install -q -U google-generativeai
 
 import google.generativeai as genai
 import textwrap
 from IPython.display import display
 from IPython.display import Markdown
 
-#Configuracao do GenerativeAI
+# Configuracao do GenerativeAI
 GOOGLE_API_KEY = "AIzaSyAzO3FR7an1eCQi_0eXuSUO9Yx2YH2cqtc"
 genai.configure(api_key=GOOGLE_API_KEY)
 
-#Setup do modelo + segurança
+# Setup do modelo + segurança
 generation_config = {
     "candidate_count": 1,
     "temperature": 0.9,
 }
 
 safety_settings = [
-  {
-    "category": "HARM_CATEGORY_HARASSMENT",
-    "threshold": "BLOCK_ONLY_HIGH"
-  },
-  {
-    "category": "HARM_CATEGORY_HATE_SPEECH",
-    "threshold": "BLOCK_ONLY_HIGH"
-  },
-  {
-    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    "threshold": "BLOCK_ONLY_HIGH"
-  },
-  {
-    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-    "threshold": "BLOCK_ONLY_HIGH"
-  },
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_ONLY_HIGH"
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_ONLY_HIGH"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_ONLY_HIGH"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_ONLY_HIGH"
+    },
 ]
 
-#Melhorando a visualização do historico
-#Código disponível em https://ai.google.dev/tutorials/python_quickstart#import_packages
-def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+# Melhorando a visualização do historico
+# Código disponível em https://ai.google.dev/tutorials/python_quickstart#import_packages
 
-#Imprimindo o histórico
+
+def to_markdown(text):
+    text = text.replace('•', '  *')
+    return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+# Imprimindo o histórico
+
+
 def show_history():
-  for message in chat.history:
-    display(to_markdown(f'**{message.role}**: {message.parts[0].text}'))
-    print('-------------------------------------------')
+    for message in chat.history:
+        display(to_markdown(f'**{message.role}**: {message.parts[0].text}'))
+        print('-------------------------------------------')
+
 
 # Preparação do modelo
 system_instruction = "Você é um chatBOT especializado em informações sobre a ONG Code Club Brasil, educação STEM, e Pensamento Computacional."
@@ -83,6 +88,6 @@ chat = model.start_chat(history=[])
 prompt = input("Digite sua pergunta: ")
 
 while prompt != "fim":
-  response = chat.send_message(prompt)
-  print(response.text, "\n")
-  prompt = input("Digite sua pergunta: ")
+    response = chat.send_message(prompt)
+    print(response.text, "\n")
+    prompt = input("Digite sua pergunta: ")
